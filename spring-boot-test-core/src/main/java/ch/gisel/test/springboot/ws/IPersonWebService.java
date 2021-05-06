8f060102ec53702b9e902cc97869ff24a47febab
Daniel <daniel@gisel.ch>
2021-05-06 21:30:04 +0200

    soap service added

---
+++ spring-boot-test-core/src/main/java/ch/gisel/test/springboot/ws/IPersonWebService.java
@@ -0,0 +1,21 @@
+package ch.gisel.test.springboot.ws;
+
+import ch.gisel.test.springboot.api.dto.PersonDTO;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+//http://localhost:8080/services/ws/person/?wsdl
+@WebService(name = "PersonWebService")
+public interface IPersonWebService {
+
+    @WebMethod
+    @WebResult(name = "out")
+    PersonDTO savePerson(@WebParam(name = "in") PersonDTO person);
+
+    @WebMethod
+    @WebResult(name = "out")
+    PersonDTO loadPerson(@WebParam(name = "id") long id);
+}
---
+++ spring-boot-test-core/src/main/java/ch/gisel/test/springboot/ws/impl/PersonWebService.java
@@ -0,0 +1,24 @@
+package ch.gisel.test.springboot.ws.impl;
+
+import ch.gisel.test.springboot.api.dto.PersonDTO;
+import ch.gisel.test.springboot.api.service.IPersonService;
+import ch.gisel.test.springboot.ws.IPersonWebService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonWebService implements IPersonWebService {
+
+    @Autowired
+    private IPersonService personService;
+
+    @Override
+    public PersonDTO savePerson(PersonDTO person) {
+        return personService.savePerson(person);
+    }
+
+    @Override
+    public PersonDTO loadPerson(long id) {
+        return personService.loadPerson(id);
+    }
+}
